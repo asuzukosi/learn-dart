6c4a1860f18da164d622d29ec2825dda93be5282
eHealth4everyone <kosisochukwu.asuzu@e4email.net>
2022-02-20 19:35:39 +0100

    feat: discover all the different datatypes in dart

--- sample-project/variables/main.dart
+++ sample-project/variables/main.dart
@@ -1,1 +1,32 @@
-void main() {}
+void main() {
+  int value = 1 + 2;
+  int value2 = 3 + 4;
+
+  int value3 = value + value2;
+  print(value);
+  print(value3);
+
+  value2 = 10;
+  print(value2);
+
+  // data  types in dart
+  // all data types in dart are classes and all
+  // variables are objects of those classes
+
+  int number = 2;
+  double price = 112.7;
+
+  num size = 32.1; // num is the parent class of int and double
+
+  String name = "Santos Enoque";
+
+  List prices = [12, 10, 32];
+
+  print(number);
+  print(price);
+
+  print(size);
+  print(name);
+
+  print(prices);
+}
